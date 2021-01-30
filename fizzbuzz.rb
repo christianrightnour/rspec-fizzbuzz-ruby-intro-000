f3ed34e1a61c2f671f8ab659c79ee951fd369919
christian Rightnour <christian_rightnour@hotmail.com>
2021-01-30 08:13:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,5 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  elsif int % 5 == 0
-    "Buzz"
-  else int % 3 and 5 ==0
-    "FizzBuzz"
   end
 end# See README.md for instructions on how to do this
