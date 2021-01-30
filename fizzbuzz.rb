7c9c3616af7531ef5b38169cd83be5d9cbda5ae7
christian Rightnour <christian_rightnour@hotmail.com>
2021-01-30 08:20:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ elsif int % 5 == 0
   "Buzz"
 elsif int % 3 == 0 and int % 5 == 0
   "nil"
-else int % [5, 3] == 0
+else int % 5, 3 == 0
   "FizzBuzz"
   end
 end# See README.md for instructions on how to do this
